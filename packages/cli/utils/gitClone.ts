081f94bef943f9425ce5293f0279b87441d29140
nixiaoyu <1149062971@qq.com>
2025-02-10 18:17:42 +0800

    feat(cli): use typescript

--- packages/cli/utils/gitClone.ts
+++ packages/cli/utils/gitClone.ts
@@ -2,10 +2,10 @@ import chalk from 'chalk';
 import ora from 'ora';
 import simpleGit from 'simple-git';
 
-export default (remote, name, option = {}) => {
+export default (remote: string, name: string, option = {}) => {
   const downSpinner = ora('正在构建...').start();
-  return new Promise((resolve, reject) => {
-    simpleGit({ timeout: 60000 }).clone(remote, name, option).then(() => {
+  return new Promise<void>((resolve, reject) => {
+    simpleGit().clone(remote, name, option).then(() => {
       downSpinner.succeed(chalk.green('构建成功！'));
       resolve();
     }).catch((err) => {
