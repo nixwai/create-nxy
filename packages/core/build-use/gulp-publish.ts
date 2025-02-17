04d672b9519c1f381936a98509470b1d4270fa06
nixiaoyu <1149062971@qq.com>
2025-02-17 15:43:02 +0800

    fix(core): change release tag name

--- packages/core/build-use/gulp-publish.ts
+++ packages/core/build-use/gulp-publish.ts
@@ -1,8 +1,8 @@
 import { series } from 'gulp';
+import { REGISTRY } from '../config';
 import { run } from '../tasks';
 import { useOutput } from './paths';
 
 export default series(
-  () => run('npm config set registry https://registry.npmjs.org'),
-  () => run('pnpm publish', useOutput),
+  () => run(`pnpm publish --registry ${REGISTRY}`, useOutput),
 );
