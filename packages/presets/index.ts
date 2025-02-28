8d0e411778041e70be757658a39264b3ac547530
nixiaoyu <1149062971@qq.com>
2025-02-28 15:46:02 +0800

    fix(preset): update preset build package

--- packages/presets/index.ts
+++ packages/presets/index.ts
@@ -2,7 +2,7 @@ import type { Theme } from '@unocss/preset-mini';
 import type { PresetOptions } from './types';
 import { definePreset } from 'unocss';
 
-export const presetMortiseTenon = definePreset<PresetOptions, Theme>((_options) => {
+export const presetCreateNxy = definePreset<PresetOptions, Theme>((_options) => {
   return { name: 'unocss-preset' };
 });
 
