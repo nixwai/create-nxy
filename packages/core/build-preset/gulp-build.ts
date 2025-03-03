2589a0fe1f0744e4b0fe3b8ebbdbfb6b71acf8ec
nixiaoyu <1149062971@qq.com>
2025-03-03 15:07:58 +0800

    feat(preset): update build config

--- packages/core/build-preset/gulp-build.ts
+++ packages/core/build-preset/gulp-build.ts
@@ -1,28 +1,8 @@
-import { resolve } from 'node:path';
 import { series } from 'gulp';
-import { copyFiles, delPath, editPackage, run } from '../tasks';
-import { presetOutput, presetRoot } from './paths';
+import { delPath, run } from '../tasks';
+import { presetOutput } from './paths';
 
 export default series(
   () => delPath(presetOutput),
   () => run('vite build'),
-  () => copyFiles(
-    presetOutput,
-    [
-      resolve(presetRoot, 'README.md'),
-      resolve(presetRoot, 'package.json'),
-    ],
-  ),
-  () => editPackage(presetOutput, (config) => {
-    config.exports = {
-      '.': {
-        types: './types/index.d.ts',
-        import: './dist/index.js',
-      },
-      './helper': {
-        types: './types/helper.d.ts',
-        import: './dist/helper.js',
-      },
-    };
-  }),
 );
